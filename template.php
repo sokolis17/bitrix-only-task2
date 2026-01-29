50f8d0cc659725b56c2d4fa29dfefefa6b1bf9c7
Igor <rononi@bk.ru>
2026-01-29 18:24:24 +0300

    Подредактировал ! ,Готово

--- template.php
+++ template.php
@@ -1,106 +1,101 @@
 <?php
-
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
-	die();
-}
-
-/**
- * @var array $arResult
- */
-
-if ($arResult["isFormErrors"] == "Y"): ?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? endif; ?>
-<?= $arResult["FORM_NOTE"] ?? '' ?>
-<? if ($arResult["isFormNote"] != "Y") {
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
-	<?= $arResult["FORM_HEADER"] ?>
-	<table>
-		<?
-		if ($arResult["isFormDescription"] == "Y" || $arResult["isFormTitle"] == "Y" || $arResult["isFormImage"] == "Y") {
-		?>
-			<tr>
-				<td><?
-					if ($arResult["isFormTitle"]) {
-					?>
-						<h3><?= $arResult["FORM_TITLE"] ?></h3>
-					<?
-					}
 
-					if ($arResult["isFormImage"] == "Y") {
-					?>
-						<a href="<?= $arResult["FORM_IMAGE"]["URL"] ?>" target="_blank" alt="<?= GetMessage("FORM_ENLARGE") ?>"><img src="<?= $arResult["FORM_IMAGE"]["URL"] ?>" <? if ($arResult["FORM_IMAGE"]["WIDTH"] > 300): ?>width="300" <? elseif ($arResult["FORM_IMAGE"]["HEIGHT"] > 200): ?>height="200" <? else: ?><?= $arResult["FORM_IMAGE"]["ATTR"] ?><? endif; ?> hspace="3" vscape="3" border="0" alt="" /></a>
+<div class="contact-form">
+	<div class="contact-form__head">
+		<div class="contact-form__head-title">Связаться</div>
+		<div class="contact-form__head-text">Наши сотрудники помогут выполнить подбор услуги и расчет цены с учетом ваших требований</div>
+	</div>
 
-					<?
-					}
-					?>
+	<? if ($arResult["isFormNote"] == "Y"): ?>
+		<div class="form-note">
+			<?= $arResult["FORM_NOTE"] ?>
+		</div>
+	<? else: ?>
 
-					<p><?= $arResult["FORM_DESCRIPTION"] ?></p>
-				</td>
-			</tr>
-		<?
-		}
-		?>
-	</table>
-	<br />
-	<table class="form-table data-table">
-		<thead>
-			<tr>
-				<th colspan="2">&nbsp;</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?
-			foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
-				if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden') {
-					echo $arQuestion["HTML_CODE"];
-				} else {
-			?>
-					<tr>
-						<td>
-							<? if (isset($arResult["FORM_ERRORS"][$FIELD_SID])): ?>
-								<span class="error-fld" title="<?= htmlspecialcharsbx($arResult["FORM_ERRORS"][$FIELD_SID]) ?>"></span>
-							<? endif; ?>
-							<?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><? endif; ?>
-							<?= $arQuestion["IS_INPUT_CAPTION_IMAGE"] == "Y" ? "<br />" . $arQuestion["IMAGE"]["HTML_CODE"] : "" ?>
-						</td>
-						<td><?= $arQuestion["HTML_CODE"] ?></td>
-					</tr>
-			<?
-				}
-			}
-			?>
-			<?
-			if ($arResult["isUseCaptcha"] == "Y") {
-			?>
-				<tr>
-					<th colspan="2"><b><?= GetMessage("FORM_CAPTCHA_TABLE_TITLE") ?></b></th>
-				</tr>
-				<tr>
-					<td>&nbsp;</td>
-					<td><input type="hidden" name="captcha_sid" value="<?= htmlspecialcharsbx($arResult["CAPTCHACode"]); ?>" /><img src="/bitrix/tools/captcha.php?captcha_sid=<?= htmlspecialcharsbx($arResult["CAPTCHACode"]); ?>" width="180" height="40" alt="" /></td>
-				</tr>
-				<tr>
-					<td><?= GetMessage("FORM_CAPTCHA_FIELD_TITLE") ?><?= $arResult["REQUIRED_SIGN"]; ?></td>
-					<td><input type="text" name="captcha_word" size="30" maxlength="50" value="" class="inputtext" /></td>
-				</tr>
-			<?
-			}
-			?>
-		</tbody>
-		<tfoot>
-			<tr>
-				<th colspan="2">
-					<input <?= (intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : ""); ?> type="submit" name="web_form_submit" value="<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>" />
-					<? if ($arResult["F_RIGHT"] >= 15): ?>
-						&nbsp;<input type="hidden" name="web_form_apply" value="Y" /><input type="submit" name="web_form_apply" value="<?= GetMessage("FORM_APPLY") ?>" />
-					<? endif; ?>
-					&nbsp;<input type="reset" value="<?= GetMessage("FORM_RESET"); ?>" />
-				</th>
-			</tr>
-		</tfoot>
-	</table>
-	<p>
-		<?= $arResult["REQUIRED_SIGN"]; ?> - <?= GetMessage("FORM_REQUIRED_FIELDS") ?>
-	</p>
-	<?= $arResult["FORM_FOOTER"] ?>
-<?
-}
+		<form class="contact-form__form" method="POST">
+			<?= bitrix_sessid_post() ?>
+
+			<div class="contact-form__form-inputs">
+				<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion): ?>
+					<? if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'text'): ?>
+						<div class="input contact-form__input">
+							<label class="input__label" for="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>">
+								<div class="input__label-text"><?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?>*<? endif ?></div>
+								<input class="input__input"
+									type="text"
+									id="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									name="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									value="<?= $arQuestion["STRUCTURE"][0]["VALUE"] ?>"
+									<? if ($arQuestion["REQUIRED"] == "Y"): ?>required="" <? endif ?>>
+								<div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+							</label>
+						</div>
+					<? endif ?>
+					<? if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'email'): ?>
+						<div class="input contact-form__input">
+							<label class="input__label" for="form_email_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>">
+								<div class="input__label-text"><?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?>*<? endif ?></div>
+								<input class="input__input"
+									type="email"
+									id="form_email_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									name="form_email_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									value="<?= $arQuestion["STRUCTURE"][0]["VALUE"] ?>"
+									<? if ($arQuestion["REQUIRED"] == "Y"): ?>required="" <? endif ?>>
+								<div class="input__notification">Неверный формат почты</div>
+							</label>
+						</div>
+					<? endif ?>
+					<? if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'tel'): ?>
+						<div class="input contact-form__input">
+							<label class="input__label" for="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>">
+								<div class="input__label-text"><?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?>*<? endif ?></div>
+								<input class="input__input"
+									type="tel"
+									id="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									name="form_text_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									value="<?= $arQuestion["STRUCTURE"][0]["VALUE"] ?>"
+									data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'"
+									maxlength="12"
+									x-autocompletetype="phone-full"
+									<? if ($arQuestion["REQUIRED"] == "Y"): ?>required="" <? endif ?>>
+								<div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+							</label>
+						</div>
+					<? endif ?>
+
+				<? endforeach ?>
+			</div>
+			<? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion): ?>
+				<? if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'textarea'): ?>
+					<div class="contact-form__form-message">
+						<div class="input">
+							<label class="input__label" for="form_textarea_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>">
+								<div class="input__label-text"><?= $arQuestion["CAPTION"] ?></div>
+								<textarea class="input__input"
+									type="text"
+									id="form_textarea_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									name="form_textarea_<?= $arQuestion['STRUCTURE'][0]['ID'] ?>"
+									value=""><?= $arQuestion["STRUCTURE"][0]["VALUE"] ?></textarea>
+								<div class="input__notification"></div>
+							</label>
+						</div>
+					</div>
+				<? endif ?>
+			<? endforeach ?>
+
+			<div class="contact-form__bottom">
+				<div class="contact-form__bottom-policy">
+					Нажимая «Отправить», Вы подтверждаете, что ознакомлены, полностью согласны и принимаете условия «Согласия на обработку персональных данных».
+				</div>
+				<button class="form-button contact-form__bottom-button"
+					data-success="Отправлено"
+					data-error="Ошибка отправки">
+					<div class="form-button__title">Оставить заявку</div>
+				</button>
+			</div>
+		</form>
+
+	<? endif ?>
+</div>
